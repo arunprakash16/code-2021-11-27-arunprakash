4f8c5d288fbc3b3fe6655ea725ffbe8a9e184780
Arun Prakash <arunprakashcs@yahoo.co.in>
2021-11-30 00:05:59 +0530

    test: add unit test

--- tests/test_pybmi.py
+++ tests/test_pybmi.py
@@ -1,1 +1,6 @@
 from pybmi import pybmi
+
+out_stat, bmi_agg = bmi_analysis('c:/test/input_file.txt')
+if out_stat == 0:
+    for bmi, bmi_cnt in bmi_agg.items():
+        print (bmi + ' : ' + str(bmi_cnt))
